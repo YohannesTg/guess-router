0280071e6f974f076f408294ffa241b4386b28b2
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-31 20:17:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://YohanzTg:Kidistmariam@cluster0.esrh3ji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -11,16 +10,25 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+exports.handler = async (event, context) => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'MongoDB connection successful' }),
+    };
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error connecting to MongoDB' }),
+    };
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
-}
-run().catch(console.dir);
+};
