4347706fc2437b539b9b236dd24ff7c4a3e1c9f7
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-31 21:28:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const { MongoClient } = require('mongodb')
-let dbConnection
-module.exports = {
-  connectToDb: (cb)=> {
-    MongoClient.connect('mongodb+srv://YohanzTg:Kidistmariam@cluster0.esrh3ji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    .then((client)=>{
-      dbConnection = client.db()
-      return cb()
-    })
-    .catch(err => {
-      console.log(err)
-      return cb(err)
-    })
-  },
-  getDb: ()=>Connection 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://YohanzTg:Kidistmariam@cluster0.esrh3ji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
+
