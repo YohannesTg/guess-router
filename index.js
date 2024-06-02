e4381245b5c668a1f30dc39daa4064ef267553e1
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-06-03 01:51:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://g-game.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
   next();
 });
 
